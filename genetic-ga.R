6308d27633ec5f7ed7805935cd0587db992f2089
Sreedhar Reddy Kakulavaram <sreedhar@Sreedhars-MacBook-Pro.local>
2017-03-10 23:58:52 -0700

    genetic project logistic

--- genetic-ga.R
+++ genetic-ga.R
@@ -1,16 +1,14 @@
+# genetic-ga.R
 bcancer <- read.csv("./../class/datasets/breast-cancer-wisconsin-data.csv",sep = ",")
-weightlimit <- 20
 c1 <- c(3:32)
 accuracy <- 0.0
 solution <- c(3:32)
 library(GA)
 library(class)
-f2 <- function(x)  {
-  print(x)
-}
+
+
 ga2 <- function() {
-min <- -10; max <- 10
-GA <- ga(type = "binary", fitness = f, nBits = 30, popSize = 50,maxiter = 20, monitor = FALSE)
+GA <- ga(type = "binary", fitness = proj_glm, nBits = 30, popSize = 50,maxiter = 30,parallel = T,keepBest = T)
 summary(GA)
 cat("accuracy \n ",accuracy)
 print("\n")
@@ -19,7 +17,8 @@ cat("solution::::: \n ",solution)
 indices2 <- x * c1
 #cat("solution variables::::: \n ",indices2)
 #print(indices2)
-knn_cancer(solution)
+#knn_cancer(solution)
+glm.verify(solution)
 }
 
 f <- function(x) {
@@ -29,7 +28,8 @@ f <- function(x) {
   #Sys.sleep(1)
   ws_train <- bcancer[1:565,indices2]
   ws_c_data <- bcancer[1:565,2]
-  ws_test   <- bcancer[566:569,3:32]
+  #ws_test   <- bcancer[566:569,3:32]
+  ws_test   <- bcancer[566:569,indices2]
   pred.results <- knn.cv(train = ws_train,cl = ws_c_data,k=10,l=4, prob = FALSE,use.all = TRUE)
   mn <- mean(pred.results == ws_c_data)
   if(!is.na(mn)) {
@@ -46,6 +46,37 @@ f <- function(x) {
   #print(length(pred.results))
   print(mn)
 }
+proj_glm <- function(x){
+  library(glmnet)
+  indices2 <- x * c1
+  print(indices2)
+  bcancer <- read.csv("datasets/breast-cancer-wisconsin-data.csv",sep = ",")
+  print(dim(bcancer))
+  #print(head(bcancer[,1:10]))
+  set.seed(222)
+  #indexes <- sample(1:nrow(bcancer),size = nrow(bcancer)) #random shuffle
+  bcancer3_data2_results <- bcancer[1:500,2]
+  #print(bcancer3_data2_results)
+  bcancer3_test_results <- bcancer[501:569,2]
+  bcancer3 <- bcancer[,indices2]
+  print(dim(bcancer3))
+  bcancer3_data2 <- bcancer3[1:500,]
+  print(dim(bcancer3_data2))
+  bcancer3_test <- bcancer3[501:569,]
+  print(dim(bcancer3_test))
+  # alpha=1 lasso
+  glm.wis3 <- glmnet(x= as.matrix(bcancer3_data2),y=as.matrix(bcancer3_data2_results),family="binomial",alpha=1)
+  #0.01 accuracy 1
+  results.wis <- predict(object=glm.wis3,s= 0.01,as.matrix(bcancer3_test),type = "response")
+  results.wis <- ifelse(results.wis >= 0.5,'M','B')
+  results.wis <- as.factor(results.wis)
+  # misClasificError <- mean(results.wis != bcancer3_test_results)
+
+  mn <- mean(results.wis == bcancer3_test_results)
+  print(paste('Accuracy',mn))
+  solution <<- x
+  return(mn)
+}
 
 knn_cancer <- function(y) {
   x <- y * c1
@@ -58,4 +89,31 @@ knn_cancer <- function(y) {
   cat(" \n length of the predicted:::",length(pred.results))
   print(mn)
   
+}
+
+glm.verify <- function(x){
+  indices2 <- x * c1
+  print(indices2)
+  bcancer <- read.csv("datasets/breast-cancer-wisconsin-data.csv",sep = ",")
+  print(dim(bcancer))
+  #print(head(bcancer[,1:10]))
+  set.seed(222)
+  #indexes <- sample(1:nrow(bcancer),size = nrow(bcancer)) #random shuffle
+  bcancer3_data2_results <- bcancer[1:500,2]
+  #print(bcancer3_data2_results)
+  bcancer3_test_results <- bcancer[501:569,2]
+  bcancer3 <- bcancer[,indices2]
+  print(dim(bcancer3))
+  bcancer3_data2 <- bcancer3[1:500,]
+  print(dim(bcancer3_data2))
+  bcancer3_test <- bcancer3[501:569,]
+  print(dim(bcancer3_test))
+  # alpha=1 lasso
+  glm.wis3 <- glmnet(x= as.matrix(bcancer3_data2),y=as.matrix(bcancer3_data2_results),family="binomial",alpha=1)
+  #0.01 accuracy 1
+  results.wis <- predict(object=glm.wis3,s= 0.01,as.matrix(bcancer3_test),type = "response")
+  results.wis <- ifelse(results.wis >= 0.5,'M','B')
+  results.wis <- as.factor(results.wis)
+  mn <- mean(results.wis == bcancer3_test_results)
+  print(paste('Accuracy',mn))
 }
